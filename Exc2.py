fbc34bf1e655f4a74bd6ac021c32567f58863bc7
OrtalYona <ortalyona94@gmail.com>
2018-01-06 14:25:25 +0200

    ortal

--- Exc2.py
+++ Exc2.py
@@ -1,16 +1,35 @@
-import Exc1
 import pandas as pd
+import csv
+import operator
+
+df = pd.read_csv('Hotels_data_Changed.csv')
+
 
 def main():
-    keep_col = ['Snapshot Date', 'Checkin Date', 'Hotel Name']#, 'DayDiff',  'Hotel Name', 'WeekDay']
-   #print(keep_col)
-    new_f = Exc1.df[keep_col]
-   #print(new_f)
+
+    keep_col = ['Snapshot Date', 'Checkin Date', 'Hotel Name', 'DayDiff', 'WeekDay', 'Discount Code', 'DiscountDiff']
+    new_f = df[keep_col].head(10)
+    print(new_f)
+
+    print("---------------------------------------------------------------")
+
     new_f.to_csv("Classification.csv")
     classi = pd.read_csv('Classification.csv')
-
     print(classi)
 
-# df.to_csv('Hotels_data_Changed.csv')
+    print("---------------------------------------------------------------")
+
+    columns = ['Snapshot Date', 'Checkin Date', 'Hotel Name', 'DayDiff', 'WeekDay', 'Discount Code']
+    new_f = new_f.groupby(columns)
+
+    print("---------------------------------------------------------------")
+
+    sort = sorted(new_f,key=operator.itemgetter(6))
+    print(sort)
+
+    new_f.to_csv("new.csv")
+    a = pd.read_csv('new.csv')
+    print(a)
+
 
 main()
