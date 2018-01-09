75910a599387d594105d45f46fc6207d3a62b93a
barbrownshtein <bar810@gmail.com>
2018-01-09 23:51:49 +0200

    fix part 2.1 and integrate with 2.2

--- Exc1.py
+++ Exc1.py
@@ -6,17 +6,17 @@ df = pd.read_csv('hotels_data.csv')
 # ADD COLUMNS DAY DIFF - THE DAYS BETWEEN SNAPSHOT DATE TO CHECKIN DATE
 df['DayDiff1'] = pd.to_datetime(df['Checkin Date'])
 df['DayDiff2'] = pd.to_datetime(df['Snapshot Date'])
-df['DayDiff']=(df['DayDiff1'] - df['DayDiff2']).dt.days
+df['DayDiff'] = (df['DayDiff1'] - df['DayDiff2']).dt.days
 del df['DayDiff1']
 del df['DayDiff2']
 
 # ADD COLUMNS WEEKDAY- FIND THE SPECIFIC DAY BY THE CHECKIN DATE
 df['WeekDay'] = pd.to_datetime(df['Checkin Date']).dt.weekday_name
 
-#ADD COLUMNS DISCOUNTDIFF - THE DIFFRENT BETWEEN ORIGINAL PRICE TO DISCOUNT PRICE
-df['DiscountDiff']= pd.to_numeric(df['Original Price'])-pd.to_numeric(df['Discount Price'])
+# ADD COLUMNS DISCOUNTDIFF - THE DIFFRENT BETWEEN ORIGINAL PRICE TO DISCOUNT PRICE
+df['DiscountDiff'] = pd.to_numeric(df['Original Price']) - pd.to_numeric(df['Discount Price'])
 
-#ADD COLUMNS DISCOUNTPERC - THE PERCENT OF THE DISCOUNT
-df['DiscountPerc']=(pd.to_numeric(df['DiscountDiff'])/pd.to_numeric(df['Original Price']))*100
+# ADD COLUMNS DISCOUNTPERC - THE PERCENT OF THE DISCOUNT
+df['DiscountPerc'] = (pd.to_numeric(df['DiscountDiff']) / pd.to_numeric(df['Original Price'])) * 100
 
-df.to_csv('Hotels_data_Changed.csv',index=False)
+df.to_csv('Hotels_data_Changed.csv', index=False)
--- Exc2.py
+++ Exc2.py
@@ -1,38 +1,117 @@
 import pandas as pd
 import pandasql as pdsql
-
+import pandas as pd
+from sklearn import tree, preprocessing
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn import tree
+from sklearn.metrics import accuracy_score
+import numpy as np
+from sklearn.metrics import confusion_matrix
+from sklearn.naive_bayes import BernoulliNB
 pysql = lambda q: pdsql.sqldf(q, globals())
 
-colnames = ['SnapshotID', 'SnapshotDate', 'CheckinDate', 'Days', 'OriginalPrice', 'DiscountPrice', 'DiscountCode', 'AvailableRooms', 'HotelName', 'HotelStars', 'DayDiff', 'WeekDay', 'DiscountDiff', 'DiscountPerc']
-df = pd.read_csv('Hotels_data_Changed.csv', header=None)
-#sprint(df)
-
-#  query = 'select *' \
-#           'from df'
-#
-# dfX = pysql(query)
-#
-# print(dfX)
-
-df2 = df[['SnapshotDate', 'CheckinDate','DiscountCode','HotelName', 'DayDiff', 'WeekDay', 'DiscountDiff']]
+# THIS PART TAKE THE WHOLE TABLE AND DROP THE UNNECCERY COLUMNS
+df = pd.read_csv('Hotels_data_Changed.csv')
+keep_col = ['Snapshot Date', 'Checkin Date', 'Discount Code', 'Hotel Name', 'DayDiff', 'WeekDay', 'DiscountDiff']
+df.rename(columns={'Snapshot Date': 'SnapshotDate'}, inplace=True)
+df.rename(columns={'Checkin Date': 'CheckinDate'}, inplace=True)
+df.rename(columns={'Discount Code': 'DiscountCode'}, inplace=True)
+df.rename(columns={'Hotel Name': 'HotelName'}, inplace=True)
 keep_col = ['SnapshotDate', 'CheckinDate', 'DiscountCode', 'HotelName', 'DayDiff', 'WeekDay', 'DiscountDiff']
-new_f = df[keep_col]
-new_f.to_csv("Classification.csv")
-Classification = pd.read_csv('Classification.csv', header=None, names=colnames)#.head(5)
-#print(Classification)
+cf = df[keep_col]
 
-df3 = Classification[['SnapshotDate', 'CheckinDate', 'DiscountCode', 'HotelName', 'DayDiff', 'WeekDay', 'DiscountDiff']]
-print('-----------------------------------print df3---------------------------------------')
-print(df3)
-print('-----------------------------------print df3---------------------------------------')
+# GROUP BY QUERY- DROP THE EXPENSIVE VECTORS
+query = 'select SnapshotDate, CheckinDate, DiscountCode, HotelName, DayDiff, WeekDay, max(DiscountDiff) from cf group by SnapshotDate, CheckinDate, HotelName, DayDiff, WeekDay'
+df = pysql(query)
 
-query = 'select SnapshotDate, CheckinDate, DiscountCode, HotelName, DayDiff, WeekDay, max(DiscountDiff) from df3 group by SnapshotDate, CheckinDate, DiscountCode, HotelName, DayDiff, WeekDay'
+# PART 2.2
+df=df.head(100)
+# GET ONLY THE FEATURES I NEED
+features = ['SnapshotDate', 'CheckinDate', 'HotelName', 'WeekDay', 'DayDiff']
 
+# DROP THE ROWS WITH MISSING VALUES
+df = df.dropna()
 
-df4 = pysql(query)
-print('-----------------------------------print df4---------------------------------------')
-print(df4)
-print('-----------------------------------print df4---------------------------------------')
-df4.to_csv("new.csv")
-# a = pd.read_csv('new.csv')
-# print(a)
+# CONVERT THE FEATUERS TO NUMERIC: to translte back: le.transform(THE FITCHER NAME)
+translate1 = lambda row: wde.transform([row])[0]
+wde = preprocessing.LabelEncoder()
+wde.fit(df['WeekDay'])
+
+translate2 = lambda row: hne.transform([row])[0]
+hne = preprocessing.LabelEncoder()
+hne.fit(df['HotelName'])
+
+translate3 = lambda row: cde.transform([row])[0]
+cde = preprocessing.LabelEncoder()
+cde.fit(df['CheckinDate'])
+
+translate4 = lambda row: sde.transform([row])[0]
+sde = preprocessing.LabelEncoder()
+sde.fit(df['SnapshotDate'])
+
+df['WeekDay'] = df['WeekDay'].apply(translate1)
+df['HotelName'] = df['HotelName'].apply(translate2)
+df['CheckinDate'] = df['CheckinDate'].apply(translate3)
+df['SnapshotDate'] = df['SnapshotDate'].apply(translate4)
+
+# INSERT THE VALUES INTO VARIABLES
+X = df[features]
+y = df["DiscountCode"]
+
+# DECISION TREE CLASSIFIER
+print("------------------------DECISION TREE-----------------------")
+print()
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1)
+model = tree.DecisionTreeClassifier()
+model.fit(X_train, y_train)
+
+# TEST THE ALGORITHEM AND SHOW STATISTICS
+y_predict = model.predict(X_test)
+
+#CONFUSION MATRIX
+matrix=pd.DataFrame(
+    confusion_matrix(y_test, y_predict),
+    columns=['Predicted 1', 'Predicted 2','Predicted 3','Predicted 4'],
+    index=['True 1', 'True 2','True 3','True 4']
+)
+print("-------------------------STATISTICS-------------------------")
+print("confusion_matrix:")
+print(matrix)
+print("------------------------------------------------------------")
+# accuracy
+accuracy=accuracy_score(y_test, y_predict)
+print("accuracy is: %s" %(accuracy))
+print("------------------------------------------------------------")
+# TP
+tp = np.diag(matrix)
+print("TP is: %s" %(tp))
+print("------------------------------------------------------------")
+# FP
+fp=matrix.sum(axis=0)-np.diag(matrix)
+print("FP:")
+print(fp)
+print("------------------------------------------------------------")
+# FN
+fn = matrix.sum(axis=1) - np.diag(matrix)
+print("FN:")
+print(fn)
+print("------------------------------------------------------------")
+# ROC
+print("ROC:")
+#TODO add this
+print("------------------------------------------------------------")
+print()
+
+#TODO fix that. its look strange
+# NAIVE BAYES CLASSIFIER
+print("-------------------------NAIVE BAYES------------------------")
+clf = GaussianNB()
+print(clf.fit(X, y))
+print(clf.predict([[10,46,274,21,4]]))
+
+dlf = BernoulliNB()
+dlf.fit(X, y)
+BernoulliNB(alpha=1.0, binarize=0.0, fit_prior=True)
+print(dlf.predict([[0,29,253,10,1]]))
