1fa8c7dcc43501863225acce78132169887974fd
Bar Brownshtein <bar810@gmail.com>
2018-02-01 11:32:28 +0200

    beauty the code

--- Exc3.py
+++ Exc3.py
@@ -17,39 +17,42 @@ def df_crossjoin(df1, df2, **kwargs):
     df2.drop('_tmpkey', axis=1, inplace=True)
     return res
 
-colnames = ['SnapshotID','SnapshotDate','CheckinDate','Days','OriginalPrice','DiscountPrice','DiscountCode','AvailableRooms',
-            'HotelName','HotelStars']
-df = pd.read_csv('hotels_data.csv', names=colnames, header=None,low_memory=False)
+
+colnames = ['SnapshotID', 'SnapshotDate', 'CheckinDate', 'Days', 'OriginalPrice', 'DiscountPrice', 'DiscountCode',
+            'AvailableRooms',
+            'HotelName', 'HotelStars']
+df = pd.read_csv('hotels_data.csv', names=colnames, header=None, low_memory=False)
 pysql = lambda q: pdsql.sqldf(q, globals())
 print("-------------------QUEREY 1-------------------")
-#150 hotels most records
+# 150 hotels most records
 q1 = 'select HotelName, count(HotelName) ' \
-            'from df '\
-            'group by HotelName '\
-            'order by count(HotelName) desc '\
-            'limit 150'
+     'from df ' \
+     'group by HotelName ' \
+     'order by count(HotelName) desc ' \
+     'limit 150'
 
 df1 = pysql(q1)
 # print(hotelNamesDf)
 print("-------------------QUEREY 2-------------------")
-#40 check in dates most records
+# 40 check in dates most records
 q2 = 'select CheckinDate, count(CheckinDate) ' \
-            'from df '\
-            'group by CheckinDate '\
-            'order by count(CheckinDate) desc '\
-            'limit 40'
+     'from df ' \
+     'group by CheckinDate ' \
+     'order by count(CheckinDate) desc ' \
+     'limit 40'
 
 df2 = pysql(q2)
-print(df2)
+# print(df2)
 
 # DROP THE UNRELEVANT DATA FROM THE MAIN DATA FRAME
-mdfq= 'select HotelName, CheckinDate,DiscountCode, min(DiscountPrice) '\
-        'from df '\
-        'where HotelName in (select HotelName from df1) '\
-        'and CheckinDate in (select CheckinDate from df2) '\
-        'group by HotelName, CheckinDate, DiscountCode'
+mdfq = 'select HotelName, CheckinDate,DiscountCode, min(DiscountPrice) ' \
+       'from df ' \
+       'where HotelName in (select HotelName from df1) ' \
+       'and CheckinDate in (select CheckinDate from df2) ' \
+       'group by HotelName, CheckinDate, DiscountCode'
 
-names = {'HotelName':'HotelName', 'CheckinDate':'CheckinDate','DiscountCode':'DiscountCode', 'min(DiscountPrice)':'DiscountPrice'}
+names = {'HotelName': 'HotelName', 'CheckinDate': 'CheckinDate', 'DiscountCode': 'DiscountCode',
+         'min(DiscountPrice)': 'DiscountPrice'}
 ndf = pysql(mdfq).rename(columns=names)
 # print(ndf)
 
@@ -58,47 +61,47 @@ df31 = pysql('select distinct DiscountCode from df where DiscountCode in (1,2,3,
 # print(df31)
 
 print("-------------------QUEREY 3.2-------------------")
-df32=df_crossjoin(df2[['CheckinDate']], df31[["DiscountCode"]])
+df32 = df_crossjoin(df2[['CheckinDate']], df31[["DiscountCode"]])
 # print(df32)
 
 print("-------------------QUEREY 3.3-------------------")
-df33=df32
-df33['datePlusCode']=df32['CheckinDate']+'_'+df32['DiscountCode']
+df33 = df32
+df33['datePlusCode'] = df32['CheckinDate'] + '_' + df32['DiscountCode']
 # print(df33)
 
 print("-------------------QUEREY 3.4-------------------")
-q34='select a.HotelName, a.DiscountPrice, b.datePlusCode '\
-    'from ndf as a '\
-    'inner join df33 as b '\
-    'on a.DiscountCode=b.DiscountCode and a.CheckinDate=b.CheckinDate '
+q34 = 'select a.HotelName, a.DiscountPrice, b.datePlusCode ' \
+      'from ndf as a ' \
+      'inner join df33 as b ' \
+      'on a.DiscountCode=b.DiscountCode and a.CheckinDate=b.CheckinDate '
 df34 = pysql(q34)
 # print(df34)
 
 print("-------------------QUEREY 4-------------------")
-df35=df34.pivot(index='HotelName', columns='datePlusCode', values='DiscountPrice')
-df35.fillna(value=-1,inplace=True) # TODO replace it to the step before.
+df35 = df34.pivot(index='HotelName', columns='datePlusCode', values='DiscountPrice')
+df35.fillna(value=-1, inplace=True)  # TODO replace it to the step before.
 df35.to_csv('pivot.csv')
 dfx = pd.read_csv('pivot.csv')
 # print(dfx)
 print("-------------------NORMALIZE-------------------")
-hotelNameList=dfx['HotelName']
-columns_names=dfx.columns.values
-dfn= dfx.drop('HotelName', 1)
-scalar=preprocessing.MinMaxScaler(copy = True, feature_range=(0, 100))
-scaled_df=scalar.fit_transform(dfn)
-scaled_df=pd.DataFrame(scaled_df)
+hotelNameList = dfx['HotelName']
+columns_names = dfx.columns.values
+dfn = dfx.drop('HotelName', 1)
+scalar = preprocessing.MinMaxScaler(copy=True, feature_range=(0, 100))
+scaled_df = scalar.fit_transform(dfn)
+scaled_df = pd.DataFrame(scaled_df)
 scaled_df.insert(0, 'HotelName', hotelNameList)
 scaled_df.to_csv('pivot_normalize.csv')
-scaled_df = pd.read_csv('pivot_normalize.csv',names=columns_names)
+scaled_df = pd.read_csv('pivot_normalize.csv', names=columns_names)
 scaled_df.drop(scaled_df.index[0], inplace=True)
 scaled_df.to_csv('pivot_normalize.csv')
 
-# print("-------------------CLUSTERING AND DENDOGRAM-------------------")
+print("-------------------CLUSTERING AND DENDOGRAM-------------------")
 scaled_df = scaled_df.set_index('HotelName')
 print()
 del scaled_df.index.name
 # Calculate the distance between each sample
 Z = hierarchy.linkage(scaled_df, 'ward')
 # Plot with Custom leaves
-hierarchy.dendrogram(Z, leaf_rotation=90, leaf_font_size=5,labels=scaled_df.index)
+hierarchy.dendrogram(Z, leaf_rotation=90, leaf_font_size=5, labels=scaled_df.index)
 plt.show()
--- Exc4based3.py
+++ Exc4based3.py
@@ -1,49 +1,50 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import udf
 from pyspark.sql.types import StringType
-from pyspark.ml.feature import Normalizer, MinMaxScaler
 from scipy.cluster import hierarchy
 from matplotlib import pyplot as plt
 import os
 
-os.environ["PYSPARK_PYTHON"]="python3"
-os.environ["PYSPARK_DRIVER_PYTHON"]="python3"
+os.environ["PYSPARK_PYTHON"] = "python3"
+os.environ["PYSPARK_DRIVER_PYTHON"] = "python3"
+
 
 def concat(value1, value2):
-   return value1 + '_' + value2
+    return value1 + '_' + value2
+
 
 spark = SparkSession.builder.appName("FinalProject").master("local[*]").getOrCreate()
 
-origDf = spark.read.csv('hotels_data.csv', header=True)\
-        .withColumnRenamed('Snapshot ID', 'SnapshotID')\
-        .withColumnRenamed('Snapshot Date', 'SnapshotDate')\
-        .withColumnRenamed('Checkin Date', 'CheckinDate')\
-        .withColumnRenamed('Original Price', 'OriginalPrice')\
-        .withColumnRenamed('Discount Price', 'DiscountPrice')\
-        .withColumnRenamed('Discount Code', 'DiscountCode')\
-        .withColumnRenamed('Available Rooms', 'AvailableRooms')\
-        .withColumnRenamed('Hotel Name', 'HotelName')\
-        .withColumnRenamed('Hotel Stars', 'HotelStars')
+origDf = spark.read.csv('hotels_data.csv', header=True) \
+    .withColumnRenamed('Snapshot ID', 'SnapshotID') \
+    .withColumnRenamed('Snapshot Date', 'SnapshotDate') \
+    .withColumnRenamed('Checkin Date', 'CheckinDate') \
+    .withColumnRenamed('Original Price', 'OriginalPrice') \
+    .withColumnRenamed('Discount Price', 'DiscountPrice') \
+    .withColumnRenamed('Discount Code', 'DiscountCode') \
+    .withColumnRenamed('Available Rooms', 'AvailableRooms') \
+    .withColumnRenamed('Hotel Name', 'HotelName') \
+    .withColumnRenamed('Hotel Stars', 'HotelStars')
 origDf.createOrReplaceTempView('origDf')
 
 hotelsDf = spark.sql('select HotelName, count(HotelName) ' \
-                        'from origDf '\
-                        'group by HotelName '\
-                        'order by count(HotelName) desc '\
-                        'limit 150')
+                     'from origDf ' \
+                     'group by HotelName ' \
+                     'order by count(HotelName) desc ' \
+                     'limit 150')
 hotelsDf.createOrReplaceTempView('hotelsDf')
 
 checkinDf = spark.sql('select CheckinDate, count(CheckinDate) ' \
-                        'from origDf '\
-                        'group by CheckinDate '\
-                        'order by count(CheckinDate) desc '\
-                        'limit 40')
+                      'from origDf ' \
+                      'group by CheckinDate ' \
+                      'order by count(CheckinDate) desc ' \
+                      'limit 40')
 checkinDf.createOrReplaceTempView('checkinDf')
 
-bestDiscountCodesDf = spark.sql('select HotelName, CheckinDate,DiscountCode, min(DiscountPrice) '\
-                                'from origDf '\
-                                'where HotelName in (select HotelName from hotelsDf) '\
-                                'and CheckinDate in (select CheckinDate from checkinDf) '\
+bestDiscountCodesDf = spark.sql('select HotelName, CheckinDate,DiscountCode, min(DiscountPrice) ' \
+                                'from origDf ' \
+                                'where HotelName in (select HotelName from hotelsDf) ' \
+                                'and CheckinDate in (select CheckinDate from checkinDf) ' \
                                 'group by HotelName, CheckinDate, DiscountCode')
 bestDiscountCodesDf = bestDiscountCodesDf.withColumnRenamed('min(DiscountPrice)', 'DiscountPrice')
 bestDiscountCodesDf.createOrReplaceTempView('bestDiscountCodesDf')
@@ -59,8 +60,8 @@ checkinDateEditedDf = spark.sql('select CheckinDate '
 checkinDateEditedDf.createOrReplaceTempView('checkinDateEditedDf')
 
 crossJoinDf = spark.sql('select a.DiscountCode as DiscountCode, b.CheckinDate as CheckinDate '
-                       'from discountCodeDf a '
-                       '    cross join checkinDateEditedDf b')
+                        'from discountCodeDf a '
+                        '    cross join checkinDateEditedDf b')
 crossJoinDf.createOrReplaceTempView('crossJoinDf')
 
 udfconcat = udf(concat, StringType())
@@ -68,15 +69,15 @@ udfconcat = udf(concat, StringType())
 crossJoinDf = crossJoinDf.withColumn('combo', udfconcat('CheckinDate', 'DiscountCode'))
 crossJoinDf.createOrReplaceTempView('crossJoinDf')
 
-prePivotDf = spark.sql('select a.HotelName, a.DiscountPrice, b.combo '\
-                        'from bestDiscountCodesDf as a '\
-                        '   inner join crossJoinDf as b '\
-                        'on a.DiscountCode=b.DiscountCode '
+prePivotDf = spark.sql('select a.HotelName, a.DiscountPrice, b.combo ' \
+                       'from bestDiscountCodesDf as a ' \
+                       '   inner join crossJoinDf as b ' \
+                       'on a.DiscountCode=b.DiscountCode '
                        '    and a.CheckinDate=b.CheckinDate ')
 
-prePivotDf = prePivotDf.withColumn('DiscountPriceInt', prePivotDf.DiscountPrice.cast('int'))\
-                        .drop('DiscountPrice')\
-                        .withColumnRenamed('DiscountPriceInt', 'DiscountPrice')
+prePivotDf = prePivotDf.withColumn('DiscountPriceInt', prePivotDf.DiscountPrice.cast('int')) \
+    .drop('DiscountPrice') \
+    .withColumnRenamed('DiscountPriceInt', 'DiscountPrice')
 
 prePivotDf.createOrReplaceTempView('prePivotDf')
 
@@ -86,26 +87,27 @@ maxPrice = int(spark.sql('select max(DiscountPrice) from prePivotDf').collect()[
 print('minPrice: ' + str(minPrice))
 print('maxPrice: ' + str(maxPrice))
 
-prePivotNormalizeDf = prePivotDf.withColumn('norm', ((prePivotDf.DiscountPrice-minPrice)/(maxPrice-minPrice) * 100).cast('double'))\
-                                .drop('DiscountPrice')\
-                                .withColumnRenamed('norm', 'DiscountPriceNormalized')
-#PIVOT
-pivotDf = prePivotNormalizeDf.groupby('HotelName')\
-                    .pivot('combo')\
-                    .avg('DiscountPriceNormalized')
+# NORMALIZE
+prePivotNormalizeDf = prePivotDf.withColumn('norm',
+                                            ((prePivotDf.DiscountPrice - minPrice) / (maxPrice - minPrice) * 100).cast(
+                                                'double')) \
+    .drop('DiscountPrice') \
+    .withColumnRenamed('norm', 'DiscountPriceNormalized')
+# PIVOT
+pivotDf = prePivotNormalizeDf.groupby('HotelName') \
+    .pivot('combo') \
+    .avg('DiscountPriceNormalized')
 pivotDf = pivotDf.fillna(-1)
 pivotDf.createOrReplaceTempView('pivotDf')
 pivotDf.show()
 
-# dendrogram
-scaled_df=pivotDf.toPandas()
+# DENDROGRAM
+scaled_df = pivotDf.toPandas()
 scaled_df = scaled_df.set_index('HotelName')
 print()
 del scaled_df.index.name
 # Calculate the distance between each sample
 Z = hierarchy.linkage(scaled_df, 'ward')
 # Plot with Custom leaves
-hierarchy.dendrogram(Z, leaf_rotation=90, leaf_font_size=5,labels=scaled_df.index)
+hierarchy.dendrogram(Z, leaf_rotation=90, leaf_font_size=5, labels=scaled_df.index)
 plt.show()
-
-
